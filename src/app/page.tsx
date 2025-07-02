3c600df2e8df7ed298de779c88385bfd082b8b31
Matúš <kollarm9@gmail.com>
2025-07-02 10:30:48 +0200

    moved location of components folder into src

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-import Header from './components/Header/Header'
+import Header from '../components/Header/Header'
 
 export const metadata: Metadata = {
   title: 'Vaše Správy',
