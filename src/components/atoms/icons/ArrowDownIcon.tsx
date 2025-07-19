5fcbfa49ad7cd011b10f809b5c49538b55aff303
Mokappa <stef@mokappa>
2025-07-19 19:13:57 +0200

    added all icons

---
+++ src/components/atoms/icons/ArrowDownIcon.tsx
@@ -0,0 +1,11 @@
+import styles from "./Icon.module.scss";
+
+const ArrowDownIcon: React.FC = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" className={styles.icon}>
+        <path d="M12 15.0538L6.34625 9.4L7.4 8.34625L12 12.9463L16.6 8.34625L17.6538 9.4L12 15.0538Z" fill="currentColor"/>
+        </svg>
+    )
+}
+
+export default ArrowDownIcon;
---
+++ src/components/atoms/icons/ArrowUpIcon.tsx
@@ -0,0 +1,11 @@
+import styles from "./Icon.module.scss";
+
+const ArrowUpIcon: React.FC = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" className={styles.icon}>
+        <path d="M12 10.4538L7.4 15.0538L6.34625 14L12 8.34625L17.6538 14L16.6 15.0538L12 10.4538Z" fill="currentColor"/>
+        </svg>
+    )
+}
+
+export default ArrowUpIcon;
