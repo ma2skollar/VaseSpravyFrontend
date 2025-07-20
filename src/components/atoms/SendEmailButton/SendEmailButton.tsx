7544c6d5ac3bbcc49f7aadbf54a8de5aeaadc74c
Mokappa <stef@mokappa>
2025-07-20 22:05:38 +0200

    added SendEmailButton, changed animation big logo VaseSpravy

---
+++ src/components/atoms/SendEmailButton/SendEmailButton.tsx
@@ -0,0 +1,21 @@
+import styles from './SendEmailButton.module.scss';
+
+type SendEmailButtonProps = {
+    text: string;
+    isDisabled: boolean;
+    onClick?: () => void;
+};
+
+const SendEmailButton: React.FC<SendEmailButtonProps> = (props) => {
+    return (
+        <button
+            className={`${styles.sendEmailButton} ${props.isDisabled ? styles.disabled : ''} link-text-sans-medium`}
+            onClick={props.onClick}
+            disabled={props.isDisabled}
+        >
+            {props.text}
+        </button>
+    );
+}
+
+export default SendEmailButton;
